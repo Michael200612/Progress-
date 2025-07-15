48a8c048d84cfdf20eacbe1f7235faa8f13e3281
Michael200612 <limbadamichael@gmail.com>
2025-07-15 15:00:57 +0200

    Update faces.py

--- Week 0/faces/faces.py
+++ Week 0/faces/faces.py
@@ -1,1 +1,9 @@
-print("\U0001F642")
+def convert(x):
+    x = x.replace(":)", "\U0001F642")
+    x = x.replace(":(", "\U0001F641")
+    return x
+
+def main():
+    print(convert(input()))
+
+main()
