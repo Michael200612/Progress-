4efcac005094236eca225d1739000ae154db3e27
Michael200612 <limbadamichael@gmail.com>
2025-07-16 14:50:19 +0200

    Update extensions.py

--- Week 1/extensions/extensions.py
+++ Week 1/extensions/extensions.py
@@ -1,10 +1,12 @@
 x = input("File name: ").strip().lower()
 y = x.index(".")
-list =  [".gif", ".jpg", ".jpeg", ".png", ".txt"]
-list2 = ["pdf", "zip"]
-if x[y:] in list:
-    print(f"image/{x[y+1:]}")
-elif x[y:] in list2:
-    print(f"application/{x[y+1:]}")
-else:
-    print("application/octet-stream")
+match x[y:]:
+    case ".gif" | ".jpg" | ".jpeg"| ".png":
+        print(f"image/{x[y+1:]}")
+    case ".pdf" | ".zip":
+        print(f"application/{x[y+1:]}")
+    case ".txt":
+        print("document.txt")
+    case _:
+        print("application/octet-stream")
+
