2a1a1ec8688d16c83c044585dccafdb989579189
Michael200612 <limbadamichael@gmail.com>
2025-07-16 14:53:59 +0200

    Update interpreter.py

--- Week 1/interpreter/interpreter.py
+++ Week 1/interpreter/interpreter.py
@@ -2,17 +2,18 @@ a = input().split()
 x = float(a[0])
 y = a[1]
 z = float(a[2])
-if y == "+":
-    print(x + z)
-elif y == "*":
-    print(x * z)
-elif y == "-":
-    print(x - z)
-elif y == "/":
-    if z == 0:
-        print("Cannot divide by zero")
-    else:
-        print(x / z)
-else:
-    print("Unsupported operator")
+match y:
+    case "+":
+       print(x + z)
+    case "*":
+       print(x * z)
+    case "-":
+       print(x - z)
+    case "/":
+       if z == 0:
+           print("Cannot divide by zero")
+       else:
+           print(x / z)
+    case _:
+       print("Unsupported operator")
 
