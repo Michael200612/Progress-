3a05cb40101bfe5d4b2ec3ea6b040dabe5342941
Michael Limbada <limbadamichael@gmail.com>
2025-08-11 15:13:02 +0200

    Week 6

--- Week 6/shirt/shirt.py
+++ Week 6/shirt/shirt.py
@@ -1,26 +1,31 @@
 import sys
-import PIL
+from PIL import Image, ImageOps
 from os.path import splitext
 
 def main():
     x,y = get()
-    read(x,y)
+    function(x,y)
 
 def get():
     if len(sys.argv) < 3:
         sys.exit("Too few command-line arguments")
     if len(sys.argv) > 3:
         sys.exit("Too many command-line arguments")
-    if not splitext(sys.argv[1])[1] in (".png",".jpeg",".jpg") or not splitext(sys.argv[2])[1] in (".png",".jpeg",".jpg"):
+    if not splitext(sys.argv[1].lower())[1] in (".png",".jpeg",".jpg") or not splitext(sys.argv[2].lower())[1] in (".png",".jpeg",".jpg"):
         sys.exit("Invalid input")
     if splitext(sys.argv[1])[1] != splitext(sys.argv[2])[1]:
         sys.exit("Input and output have different extensions")
     return sys.argv[1], sys.argv[2]
 
-def read(x,y):
-    Pil.Image.open(x)
-
-def write():
-    ...
+def function(x,y):
+    try:
+        img = Image.open(x)
+    except FileNotFoundError:
+        sys.exit("Input does not exist")
+    shirt = Image.open("shirt.png")
+    size = shirt.size
+    m = ImageOps.fit(img, size)
+    m.paste(shirt, shirt)
+    m.save(y)
 
 main()
